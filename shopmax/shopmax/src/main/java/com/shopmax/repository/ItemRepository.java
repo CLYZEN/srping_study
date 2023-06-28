14a9594caaaba2b8e1b6b911b2f5675cfbffab49
CLYZEN <jimuny01@gmail.com>
2023-06-28 17:57:47 +0900

    230628 commit

--- shopmax/shopmax/src/main/java/com/shopmax/repository/ItemRepository.java
+++ shopmax/shopmax/src/main/java/com/shopmax/repository/ItemRepository.java
@@ -1,9 +1,54 @@
 package com.shopmax.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDateTime;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.shopmax.constant.ItemSellStatus;
 import com.shopmax.entity.Item;
 
 public interface ItemRepository extends JpaRepository<Item, Long>{ // <해당 repo에서 사용할 엔티티클래스명 , PK 타입>
-
+	// select * from item where item_no = ?
+	List<Item> findByItemNm(String itemNm);
+	
+	// select * from item where item_nm ? and item_sell_status = ?
+	// 퀴즈 1-1
+	List<Item> findByItemNmAndItemSellStatus(String itemNm, ItemSellStatus itemSellStatus);
+	
+	// 퀴즈 1-2
+	List<Item> findByPriceBetween(int a, int b);
+	
+	// 퀴즈 1-3
+	List<Item> findByRegTimeAfter(LocalDateTime date);
+	
+	// 퀴즈 1-4
+	List<Item> findByItemSellStatusNotNull();
+	
+	// 퀴즈 1-5
+	List<Item> findByItemDetailEndingWith(String param);
+	
+	// 퀴즈 1-6
+	List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
+	
+	// 퀴즈 1-7
+	List<Item> findByPriceBeforeOrderByPriceDesc(int price);
+	
+	// JPQL(non native query) -> 컬럼명, 테이블명은 엔티티클래스 기준
+	@Query(value = "select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
+	List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
+	
+	// JPQL(native query) -> h2 DB 기준 쿼리작성
+	@Query(value = "select * from item where item_detail like %:itemDetail% order by price desc", nativeQuery = true)
+	List<Item> findByItemDetailByNative(@Param("itemDetail") String itemDetail);
+	
+	// 퀴즈 2-1
+	@Query("select i from Item i where i.price >= :price")
+	List<Item> findByPirceOver(@Param("price") int price);
+	
+	// 퀴즈 2-2
+	@Query("select i from Item i where i.itemNm = :itemName and i.itemSellStatus = :itemSellStatus")
+	List<Item> findItemNmStatus(@Param("itemName") String itemName, @Param("itemSellStatus") ItemSellStatus itemSellStatus);
 }
--- shopmax/shopmax/src/test/java/com/shopmax/repository/ItemRepositoryTest.java
+++ shopmax/shopmax/src/test/java/com/shopmax/repository/ItemRepositoryTest.java
@@ -1,14 +1,208 @@
 package com.shopmax.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
+import com.shopmax.constant.ItemSellStatus;
+import com.shopmax.entity.Item;
+
+@SpringBootTest // bean 객체로 만듬 -> 스프링 컨테이너에 등록
+@TestPropertySource(locations = "classpath:application-test.properties")
 class ItemRepositoryTest {
 
+	@Autowired
+	ItemRepository itemRepository;
+
+	@Disabled
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	@DisplayName("상품 저장 테스트")
+	public void createItemTest() {
+		Item item = new Item();
+		item.setItemNm("테스트 상품");
+		item.setPrice(10000);
+		item.setItemDetail("테스트 상품 상세 설명");
+		item.setItemSellStatus(ItemSellStatus.SELL);
+		item.setStockNumber(100);
+		item.setRegTime(LocalDateTime.now());
+		item.setUpdateTime(LocalDateTime.now());
+
+		// insert
+		Item savedItem = itemRepository.save(item);
+		System.out.println(savedItem.toString());
 	}
 
+	public void createItemList() {
+
+		for (int i = 1; i <= 10; i++) {
+			Item item = new Item();
+			item.setItemNm("테스트 상품" + i);
+			item.setPrice(10000 + i);
+			item.setItemDetail("테스트 상품 상세 설명" + i);
+			item.setItemSellStatus(ItemSellStatus.SELL);
+			item.setStockNumber(100);
+			item.setRegTime(LocalDateTime.now());
+			item.setUpdateTime(LocalDateTime.now());
+
+			// insert
+			itemRepository.save(item);
+		}
+	}
+
+	@Disabled
+	@Test
+	@DisplayName("상품 조회 테스트")
+	public void findByItemNmTest() {
+		this.createItemList(); // 데이터 10개 insert
+
+		List<Item> itemList = itemRepository.findByItemNm("테스트 상품1");
+
+		for (Item item : itemList) {
+			System.out.println(item.toString());
+		}
+	}
+
+	@Disabled
+	@Test
+	@DisplayName("퀴즈 1-1")
+	public void quiz1_1Test() {
+		this.createItemList();
+		List<Item> itemList = itemRepository.findByItemNmAndItemSellStatus("테스트 상품1", ItemSellStatus.SELL);
+
+		for (Item item : itemList) {
+			System.out.println(item.toString());
+		}
+	}
+
+	@Disabled
+	@Test
+	@DisplayName("퀴즈 1-2")
+	public void quiz1_2Test() {
+		this.createItemList();
+		List<Item> itemList = itemRepository.findByPriceBetween(10004, 10008);
+
+		for (Item item : itemList) {
+			System.out.println(item.toString());
+		}
+	}
+
+	@Disabled
+	@Test
+	@DisplayName("퀴즈 1-3")
+	public void quiz1_3Test() {
+		this.createItemList();
+		LocalDateTime date = LocalDateTime.of(LocalDate.of(2023, 1, 1), LocalTime.of(12, 12, 44));
+
+		System.out.println(date.toString());
+		List<Item> itemList = itemRepository.findByRegTimeAfter(date);
+
+		for (Item item : itemList) {
+			System.out.println(item.toString());
+		}
+	}
+
+	@Disabled
+	@Test
+	@DisplayName("퀴즈 1-4")
+	public void quiz1_4Test() {
+		this.createItemList();
+		List<Item> itemList = itemRepository.findByItemSellStatusNotNull();
+
+		for (Item item : itemList) {
+			System.out.println(item.toString());
+		}
+	}
+
+	@Disabled
+	@Test
+	@DisplayName("퀴즈 1-5")
+	public void quiz1_5Test() {
+		this.createItemList();
+		List<Item> itemList = itemRepository.findByItemDetailEndingWith("1");
+
+		for (Item item : itemList) {
+			System.out.println(item.toString());
+		}
+	}
+	
+	@Disabled
+	@Test
+	@DisplayName("퀴즈 1-6")
+	public void quiz1_6Test() {
+		this.createItemList();
+		List<Item> itemList = itemRepository.findByItemNmOrItemDetail("테스트 상품1", "테스트 상품 상세 설명5");
+
+		for (Item item : itemList) {
+			System.out.println(item.toString());
+		}
+	}
+
+	@Disabled
+	@Test
+	@DisplayName("퀴즈 1-7")
+	public void quiz1_7Test() { 
+		this.createItemList();
+		List<Item> itemList = itemRepository.findByPriceBeforeOrderByPriceDesc(10005);
+		 
+		for(Item item : itemList) { 
+			System.out.println(item.toString()); 
+	 	}
+	 }
+	 
+	@Disabled
+	@Test
+	@DisplayName("@Query를 이용한 상품 조회 테스트")
+	public void findByItemDetailTest() {
+		this.createItemList();
+		List<Item> itemList = itemRepository.findByItemDetail("테스트 상품 상세");
+		
+		for(Item item : itemList) { 
+			System.out.println(item.toString()); 
+	 	}
+	}
+	
+	@Disabled
+	@Test
+	@DisplayName("@Query를 이용한 상품 조회 테스트 native query")
+	public void findByItemDetaiByNativelTest() {
+		this.createItemList();
+		List<Item> itemList = itemRepository.findByItemDetailByNative("테스트 상품 상세");
+		
+		for(Item item : itemList) { 
+			System.out.println(item.toString()); 
+		}
+	}
+	
+	@Disabled
+	@Test
+	@DisplayName("퀴즈 2-1")
+	public void findByPirceOverTest() {
+		this.createItemList();
+		List<Item> itemList = itemRepository.findByPirceOver(10005);
+		
+		for(Item item : itemList) { 
+			System.out.println(item.toString()); 
+		}
+	}
+	
+	@Disabled
+	@Test
+	@DisplayName("퀴즈 2-2")
+	public void findItemNmStatus() {
+		this.createItemList();
+		List<Item> itemList = itemRepository.findItemNmStatus("테스트 상품1", ItemSellStatus.SELL);
+		
+		for(Item item : itemList) { 
+			System.out.println(item.toString()); 
+		}
+	}
+	
 }
